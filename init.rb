d5cf21348d6afa5fd93a8da13c796fba06c885e2
Pavel Tacid <tacid@tacid.kiev.ua>
2015-08-28 15:48:27 +0300

    new plugin skelet

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_print_issue do
+  name 'Redmine Print Issue plugin'
+  author 'Tacid'
+  description 'This is a plugin for printing issue'
+  version '0.0.1'
+  url 'http://github.com/tacid/redmine_print_issue'
+  author_url 'https://github.com/tacid'
+end
