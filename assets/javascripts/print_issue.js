37b66cf5781acba2f5e9ad65fc3659994a6c49c3
Pavel Tacid <tacid@tacid.kiev.ua>
2015-08-29 01:49:46 +0300

    add: print icon, some changes to scripts

--- assets/javascripts/print_issue.js
+++ assets/javascripts/print_issue.js
@@ -1,4 +1,4 @@
 jQuery(document).ready(function(){
-  var printLink = $('<a href="#" class="icon icon-copy">Print as Story</a>').on('click', function(){window.print()});
+  var printLink = $('<a href="#" class="icon icon-print">Print</a>').on('click', function(){window.print()});
   $('#content .description > .contextual').append(printLink);
 });
