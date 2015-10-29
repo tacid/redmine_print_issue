1c1ca912895b853da02d97f464db5a3370cbe9d8
Pavel Tacid <tacid@tacid.kiev.ua>
2015-10-29 14:48:07 +0300

    removed permissions from init, fixed permissions for including plugin to issue page

--- init.rb
+++ init.rb
@@ -4,14 +4,14 @@ Redmine::Plugin.register :redmine_print_issue do
   name 'Redmine Print Issue plugin'
   author 'Tacid'
   description 'This is a plugin for printing issue description with template'
-  version '1.2.1'
+  version '1.2.2'
   url 'https://github.com/tacid/redmine_print_issue'
   author_url 'https://github.com/tacid'
   settings default: {
     print_tracker_ids: Tracker.all.map{|t| t.id.to_s},
     print_templates: [ { name: "Default", template: '' } ]
   }, partial: 'settings/redmine_print_issue_settings'
-  project_module :issue_tracking do
-    permission :render_print_template, issues: [ :render_print_template ],  public: true
-  end
+  #project_module :issue_tracking do
+  #  permission :render_print_template, public: true
+  #end
 end
